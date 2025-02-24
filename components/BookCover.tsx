ad2b8e346bdf726de879f057a2a3a9835ceaa714
Nadya <nb1985@ukr.net>
2025-02-24 10:23:03 +0200

    feat: add BookList component with latest popular book; set isLoanedBook to true for the first book in sampleBooks array

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -10,7 +10,7 @@ const variantStyles: Record<BookCoverVariant, string> = {
   small: "book-cover_small",
   medium: "book-cover_medium",
   regular: "book-cover_regular",
-  wide: "book-cover_wide",
+  wide: "book-cover_wide"
 };
 
 
@@ -25,6 +25,7 @@ const BookCover = ({ variant="regular",
                      className, 
                      coverColor="#012B48",
                      coverUrl = "https://placehold.co/400x600.png"} : Props) => {
+                      
   return (
     <div className={cn("relative transition-all duration-300",
         variantStyles[variant],
@@ -34,7 +35,7 @@ const BookCover = ({ variant="regular",
       <BookCoverSvg coverColor={coverColor}/>
 
       <div className="absolute z-10" 
-           style={{ left: "12%", width: "87.5%", height: "88%"}}>
+          style={{ left: "12%", width: "87.5%", height: "88%"}} >
         <Image src={coverUrl} alt="Book cover" fill className="rounded-sm object-fill"/>
       </div>    
     </div>
