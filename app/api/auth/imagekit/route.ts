f6824f01734e44ffb4cdaa439b906c2684160ef8
Nadya <nb1985@ukr.net>
2025-02-26 17:32:27 +0200

    feat(api): add authentication route to generate ImageKit auth parameters

---
+++ app/api/auth/imagekit/route.ts
@@ -0,0 +1,26 @@
+import ImageKit from "imagekit";
+import config from "@/lib/config";
+import { NextResponse } from "next/server";
+
+
+//Next.js backend 
+
+const { publicKey, urlEndpoint, privateKey } = config.env.imagekit;
+
+const imagekit = new ImageKit({
+    publicKey,
+    urlEndpoint,
+    privateKey,
+});
+
+export async function GET(){
+    return NextResponse.json(imagekit.getAuthenticationParameters());
+
+    /*answer is object
+    {
+        "token": "random-token-string",
+        "expire": 1700000000, 
+        "signature": "hashed-signature"
+    }*/
+      
+};
