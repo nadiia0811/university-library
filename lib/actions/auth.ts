9c0b645bc4877ffd6b1501f9349cb33aad86842f
Nadya <nb1985@ukr.net>
2025-03-05 14:51:22 +0200

    feat(auth): set rate limit on signUp and signIn to protect against high request volume

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -6,11 +6,19 @@ import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import ratelimit from "../ratelimit";
+import { redirect } from "next/navigation";
 
 
 
 export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
   const { email, password } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  
+  const { success } = await ratelimit.limit(ip);
+  if (!success) redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -33,6 +41,12 @@ export const signUp = async (params: AuthCredentials) => {
           password, 
           universityId, 
           universityCard } = params;
+
+  //access to user's ip
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  
+  const { success } = await ratelimit.limit(ip);
+  if (!success) redirect("/too-fast");
 
   //check if user exists
   const existingUser = await db.select()  //array with 1 object
