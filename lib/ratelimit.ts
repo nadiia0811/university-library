b47a744230b1ed9c46c027e54ba193c824cee064
Nadya <nb1985@ukr.net>
2025-03-05 14:28:52 +0200

    feat(rateLimit): create instance to prevent DDoS and excessive requests

---
+++ lib/ratelimit.ts
@@ -0,0 +1,14 @@
+import redis from "@/database/redis";
+import { Ratelimit } from "@upstash/ratelimit"; 
+
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.fixedWindow(10, "1m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
+
+export default ratelimit;
