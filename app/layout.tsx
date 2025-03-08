47ad453a28a424cc15bd05ee3b3668285a429891
Nadya <nb1985@ukr.net>
2025-03-08 18:26:28 +0200

    fix: correct 'Semibold' to 'SemiBold'

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const ibmPlexSans = localFont({  //defining custom font
   src: [
     {path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal"},
     {path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal"},
-    {path: "/fonts/IBMPlexSans-Semibold.ttf", weight: "600", style: "normal"},
+    {path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal"},
     {path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal"},
   ]
 });
