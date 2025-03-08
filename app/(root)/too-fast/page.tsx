3ebda9d6a09fdd4c7c451e624d7022e3b4161eab
Nadya <nb1985@ukr.net>
2025-03-08 14:32:09 +0200

    feat: add page for users exceeding request limit

---
+++ app/(root)/too-fast/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="root-container flex items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Whoa, Slow Down There!
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        You're sending too many requests in a short period of time. Please slow
+        down and try again later. Thank you for your patience!
+      </p>
+    </main>
+  );
+};
+
+export default Page;
