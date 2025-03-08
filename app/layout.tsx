6ce07cab4fb89fe001e7f07c6f97f6c1cbdf537c
Nadya <nb1985@ukr.net>
2025-03-08 17:59:34 +0200

    chore: update font paths for correct resolution

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({  //defining custom font
   src: [
-    {path: "./fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal"},
-    {path: "./fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal"},
-    {path: "./fonts/IBMPlexSans-Semibold.ttf", weight: "600", style: "normal"},
-    {path: "./fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal"},
+    {path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal"},
+    {path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal"},
+    {path: "/fonts/IBMPlexSans-Semibold.ttf", weight: "600", style: "normal"},
+    {path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal"},
   ]
 });
 
