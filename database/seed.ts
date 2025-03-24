c1d9cd97cbf56f361cf6a1a787df934ea4b04aaf
Nadya <nb1985@ukr.net>
2025-03-24 19:27:08 +0200

    feat: add seed function to batch upload books to DB

---
+++ database/seed.ts
@@ -0,0 +1,65 @@
+import dummyBooks from "@/dummybooks.json";
+import ImageKit from "imagekit";
+import { books } from "./schema";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { config } from "dotenv";
+
+config({ path: ".env"});
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({client: sql});
+const imageKit = new ImageKit({
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+});
+
+const uploadToImageKit = async (
+  url: string,
+  fileName: string,
+  folder: string
+) => {
+  try {
+    const response = await imageKit.upload({
+      file: url,
+      fileName,
+      folder
+    });
+
+    return response.filePath;
+  } catch (error) {
+    console.log("Error uploading image to ImageKit: ", error);
+  }
+};
+
+const seed = async () => {
+  console.log("Seeding data...");
+
+  try {
+    for (const book of dummyBooks) {
+      const coverUrl = await uploadToImageKit(
+        book.coverUrl,
+        `${book.title}.jpg`,
+        "/books/covers"
+      ) as string;
+      
+      /*const videoUrl = await uploadToImageKit(
+        book.videoUrl,
+        `${book.title}.mp4`,
+        "/books/videos"
+      );*/
+
+      await db.insert(books).values({
+        ...book,
+        coverUrl
+      });
+
+      console.log("Data seeded successfully");
+    } 
+  } catch (error) {
+    console.log("Error seedind data: ", error);
+  }
+};
+
+seed();
