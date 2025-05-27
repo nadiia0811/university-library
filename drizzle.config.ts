b576027c1caecf7c85c62ee1e92bb79b30b79e73
Nadya <nb1985@ukr.net>
2025-05-27 17:48:11 +0300

    chore(drizzle.config): use default export instead of defaultConfig

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
 import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
-
 config({ path: ".env" });
 
-export default defineConfig({
+export default {
   schema: "./database/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
+
